1b8ffff526eef302395f685be7c3889934708289
Niraj Mehta <nirajmehta960@gmail.com>
2025-09-28 14:53:12 -0400

    feat: Styling kambaz navigation sidebar

--- app/(Kambaz)/layout.tsx
+++ app/(Kambaz)/layout.tsx
@@ -1,13 +1,16 @@
 import { ReactNode } from "react";
 import KambazNavigation from "./Navigation";
+import "./styles.css";
 export default function KambazLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
   return (
     <div id="wd-kambaz">
       <div className="d-flex">
-        <KambazNavigation />
-        <div className="flex-fill">{children}</div>
+        <div>
+          <KambazNavigation />
+        </div>
+        <div className="wd-main-content-offset p-3 flex-fill">{children}</div>
       </div>
     </div>
   );
