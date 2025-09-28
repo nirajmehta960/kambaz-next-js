1bb62cf00d310ee38677bd77ba0703109a67a70d
Niraj Mehta <nirajmehta960@gmail.com>
2025-09-28 15:35:53 -0400

    feat: Styling the Course Navigation Sidebar

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -6,39 +6,63 @@ export default function CourseNavigation() {
   const params = useParams();
   const cid = params.cid;
   return (
-    <div id="wd-courses-navigation">
-      <Link href={`/Courses/${cid}/Home`} id="wd-course-home-link">
+    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+      <Link
+        href={`/Courses/${cid}/Home`}
+        id="wd-course-home-link"
+        className="list-group-item active border-0"
+      >
         Home
       </Link>
-      <br />
-      <Link href={`/Courses/${cid}/Modules`} id="wd-course-modules-link">
+      <Link
+        href={`/Courses/${cid}/Modules`}
+        id="wd-course-modules-link"
+        className="list-group-item text-danger border-0"
+      >
         Modules
       </Link>
-      <br />
-      <Link href={`/Courses/${cid}/Piazza`} id="wd-course-piazza-link">
+      <Link
+        href={`/Courses/${cid}/Piazza`}
+        id="wd-course-piazza-link"
+        className="list-group-item text-danger border-0"
+      >
         Piazza
       </Link>
-      <br />
-      <Link href={`/Courses/${cid}/Zoom`} id="wd-course-zoom-link">
+      <Link
+        href={`/Courses/${cid}/Zoom`}
+        id="wd-course-zoom-link"
+        className="list-group-item text-danger border-0"
+      >
         Zoom
       </Link>
-      <br />
-      <Link href={`/Courses/${cid}/Assignments`} id="wd-course-assignments-link">
+      <Link
+        href={`/Courses/${cid}/Assignments`}
+        id="wd-course-assignments-link"
+        className="list-group-item text-danger border-0"
+      >
         Assignments
       </Link>
-      <br />
-      <Link href={`/Courses/${cid}/Quizzes`} id="wd-course-quizzes-link">
+      <Link
+        href={`/Courses/${cid}/Quizzes`}
+        id="wd-course-quizzes-link"
+        className="list-group-item text-danger border-0"
+      >
         Quizzes
       </Link>
-      <br />
-      <Link href={`/Courses/${cid}/Grades`} id="wd-course-grades-link">
+      <Link
+        href={`/Courses/${cid}/Grades`}
+        id="wd-course-grades-link"
+        className="list-group-item text-danger border-0"
+      >
         Grades
       </Link>
-      <br />
-      <Link href={`/Courses/${cid}/People/Table`} id="wd-course-people-link">
+      <Link
+        href={`/Courses/${cid}/People/Table`}
+        id="wd-course-people-link"
+        className="list-group-item text-danger border-0"
+      >
         People
       </Link>
-      <br />
     </div>
   );
 }
