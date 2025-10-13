eee6e09d1f42dbf3e5b168e8d706cdceef170f19
Niraj Mehta <nirajmehta960@gmail.com>
2025-10-12 23:39:22 -0400

    Fixing unescaped quotes error to resolve vercel conflicts

--- app/Labs/Lab3/Destructing.tsx
+++ app/Labs/Lab3/Destructing.tsx
@@ -9,7 +9,8 @@ export default function Destructing() {
     <div id="wd-destructing">
       <h2>Destructing</h2>
       <h3>Object Destructing</h3>
-      const &#123; name, age &#125; = &#123; name: "John", age: 25 &#125;
+      const &#123; name, age &#125; = &#123; name: &quot;John&quot;, age: 25
+      &#125;
       <br />
       <br />
       name = {name}
