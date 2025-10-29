8222300aa14601495579d3073eb3e4636c67c06a
Niraj Mehta <nirajmehta960@gmail.com>
2025-10-29 11:28:25 -0400

    Adding State Management to the Modules Screen

--- app/(Kambaz)/store.tsx
+++ app/(Kambaz)/store.tsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import coursesReducer from "./Courses/reducer";
+import modulesReducer from "./Courses/[cid]/Modules/reducer";
 const store = configureStore({
-  reducer: { coursesReducer },
+  reducer: { coursesReducer, modulesReducer },
 });
 export default store;
